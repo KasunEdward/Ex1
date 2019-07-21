d87d177af7c124fa7e360ec38eb6ac76111811f6
Kasun <kasun@omobio.net>
2019-07-21 14:15:29 +0530

    Changes to migration file tocreate user table
    To create table run mix ecto.migrate

--- priv/repo/migrations/20190721082644_create_users.exs
+++ priv/repo/migrations/20190721082644_create_users.exs
@@ -2,6 +2,10 @@ defmodule Ex1.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
-
+    create table(:user) do
+      add :name, :string
+      add :age, :integer
+      add :status, :integer
+    end
   end
 end
